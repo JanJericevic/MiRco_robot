fbcc04a60bea26d960779a1844d482dea0dad751
Jan Jericevic <jan.jericevic@gmail.com>
2024-07-27 17:31:39 +0200

    removed unnecessary files

--- convenience_scripts/container_prompt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#open another interactive bash prompt of running container
-#for now only works for one running container
-#TODO: add autocompletion or variable input
-docker exec -it $(docker ps -aq) /bin/bash 
